b7d47952a9144be7db1c64ac837c85ec9ecea396
Ingo Ruhnke <grumbel@gmail.com>
2024-05-26 17:50:12 +0200

    Update to nixos-24.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     wxedid_src.url = "https://sourceforge.net/projects/wxedid/files/wxedid-0.0.29.tar.gz";
